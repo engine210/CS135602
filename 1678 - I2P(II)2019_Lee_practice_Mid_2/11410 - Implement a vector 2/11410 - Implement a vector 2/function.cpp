7eca2c95ae38d4e99e496d67dcf4ed49db41e018
Steven Wang <engine210@gmail.com>
2019-05-12 19:11:46 +0800

    pass implement vector

--- 1678 - I2P(II)2019_Lee_practice_Mid_2/11410 - Implement a vector 2/11410 - Implement a vector 2/function.cpp
+++ 1678 - I2P(II)2019_Lee_practice_Mid_2/11410 - Implement a vector 2/11410 - Implement a vector 2/function.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "function.hpp"
+#include <iostream>
 
 oj::Vector::Vector() {
     begin_ = nullptr;
@@ -21,31 +22,11 @@ void oj::Vector::pop_back() {
 }
 
 void oj::Vector::push_back(data_t val) {
-    size_t oldCapacity = capacity();
-    size_t oldSize = size();
-    if (oldCapacity == oldSize) {
-        pointer newbegin;
-        if (capacity() + 1 > capacity() * 3) {
-            newbegin = new Int[oldCapacity+1];
-            end_ = newbegin + oldCapacity + 1;
-        }
-        else {
-            newbegin = new Int[oldCapacity*3];
-            end_ = newbegin + oldCapacity*3;
-        }
-        int i;
-        for (i = 0; i < oldCapacity; i++) {
-            newbegin[i] = begin_[i];
-        }
-        newbegin[i] = val;
-        delete [] begin_;
-        begin_ = newbegin;
-        last_ = begin_ + oldCapacity + 1;
+    if (size() == capacity()) {
+        reserve(std::max(capacity()+1, capacity()*3));
     }
-    else {
-        *last_ = val;
-        last_++;
-    }
+    *last_ = val;
+    last_++;
 }
 
 size_t oj::Vector::size() const {
@@ -57,43 +38,16 @@ size_t oj::Vector::capacity() const {
 }
 
 void oj::Vector::insert(std::size_t pos, std::size_t count, data_t val) {
-    size_t oldCapacity = capacity();
-    size_t oldSize = size();
-    if (oldSize + count > oldCapacity) {
-        pointer newbegin;
-        if (oldCapacity * 1.5 > oldCapacity + count) {
-            newbegin = new Int[static_cast<int>(oldCapacity * 1.5)];
-            end_ = newbegin + static_cast<int>(oldCapacity * 1.5);
-        }
-        else {
-            newbegin = new Int[oldCapacity + count];
-            end_ = newbegin + oldCapacity + count;
-        }
-        
-        int i, j;
-        for (i = 0; i <= pos; i++) {
-            newbegin[i] = begin_[i];
-        }
-        j = i;
-        for (; i <= pos + count; i++) {
-            newbegin[i] = val;
-        }
-        for (; j < oldSize; j++, i++) {
-            newbegin[i] = begin_[j];
-        }
-        delete [] begin_;
-        begin_ = newbegin;
-        last_ = begin_ + oldSize + count;
+    if (size() + count > capacity()) {
+        reserve(std::max(static_cast<size_t>(capacity()*1.5), capacity()+count));
     }
-    else {
-        for (auto i = oldSize + count - 1; i >= pos + count; i--) {
-            begin_[i] = begin_[i - count];
-        }
-        for (auto i = pos; i < pos + count; i++) {
-            begin_[i] = val;
-        }
-        last_ = begin_ + oldSize + count;
+    for (auto i = size() + count - 1; i >= pos + count; i--) {
+        begin_[i] = begin_[i-count];
     }
+    for (auto i = pos; i < pos+count; i++) {
+        begin_[i] = val;
+    }
+    last_ += count;
 }
 
 void oj::Vector::reserve(std::size_t new_capacity) {
@@ -103,7 +57,7 @@ void oj::Vector::reserve(std::size_t new_capacity) {
         pointer newbegin = new Int[new_capacity];
         end_ = newbegin + new_capacity;
         int i;
-        for (i = 0; i < oldCapacity; i++) {
+        for (i = 0; i < oldSize; i++) {
             newbegin[i] = begin_[i];
         }
         delete [] begin_;
@@ -116,24 +70,18 @@ void oj::Vector::resize(std::size_t new_size) {
     size_t oldCapacity = capacity();
     size_t oldSize = size();
     if (new_size > oldCapacity) {
-        pointer newbegin = new Int[new_size];
-        end_ = newbegin + new_size;
-        int i;
-        for (i = 0; i < oldCapacity; i++) {
-            newbegin[i] = begin_[i];
+        reserve(new_size);
+        for (auto i = oldSize; i < new_size; i++) {
+            begin_[i] = 0;
         }
-        for (; i < new_size; i++) {
-            newbegin[i] = 0;
-        }
-        delete [] begin_;
-        begin_ = newbegin;
-        last_ = end_;
+        last_ = begin_ + new_size;
     }
     else if (new_size > oldSize) {
-        for (int i = 0; i < new_size - oldSize; i++) {
-            *last_ = 0;
-            last_++;
+        size_t oldSize = size();
+        for (auto i = oldSize; i < new_size; i++) {
+            begin_[i] = 0;
         }
+        last_ = begin_ + new_size;
     }
     else if (new_size < oldSize){
         last_ -= oldSize - new_size;
